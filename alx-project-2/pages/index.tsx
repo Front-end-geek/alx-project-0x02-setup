9eedddd70abe771b692ebac282f834deee8fb723
front-end-geek <lovepreetbrands@gmail.com>
2025-01-13 13:44:58 +0300

    task 1

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
+      <h1>Welcome to My Website</h1>
+      <p>Explore and discover amazing projects and content.</p>
+    </div>
+  );
+};
+
+export default Home;
